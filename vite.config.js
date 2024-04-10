4cc9863c5cb097a58ba262b2368a5af8cab7aee5
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2024-04-10 19:48:15 -0300

    uninstall gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://nicolassequeira11.github.io/moviecenter/",
   plugins: [react()],
 })
